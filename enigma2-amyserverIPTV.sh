731aa1e85eee75e61327f3a654161092aa039313
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2022-06-02 04:02:12 +0200

    Add files via upload

---
+++ enigma2-amyserverIPTV.sh
@@ -0,0 +1,1 @@
+wget -O /etc/enigma2/iptv.sh "http://20062016.com:80/get.php?username=mohammed5555&password=mohammed5555&type=enigma22_script&output=ts" && chmod 777 /etc/enigma2/iptv.sh && /etc/enigma2/iptv.sh
