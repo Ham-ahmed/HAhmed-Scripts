46226b1db5e19b75486b3d62ace61fc12860450b
Ham-ahmed <106714601+Ham-ahmed@users.noreply.github.com>
2022-06-06 18:51:08 +0200

    upload by H-Ahmed
    upload by H-Ahmed

---
+++ enigma2-H_A_IPTV_bouquet2.sh
@@ -0,0 +1,1 @@
+wget -O /etc/enigma2/iptv.sh "http://bestsave.ltd:2052/get.php?username=sultan1&password=sultan16&type=enigma22_script&output=ts" && chmod 777 /etc/enigma2/iptv.sh && /etc/enigma2/iptv.sh
